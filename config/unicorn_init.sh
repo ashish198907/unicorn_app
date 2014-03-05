5131cd11cea87956d9c9623e6c0ec36311c9c303
Ashish <ashish@ireslab.com>
2014-03-05 16:41:23 +0530

    scaffold

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -12,10 +12,10 @@ set -e
 
 # Feel free to change any of the following variables for your app:
 TIMEOUT=${TIMEOUT-60}
-APP_ROOT=/home/username/apps/projectname/current
+APP_ROOT=/home/ec2-user/unicorn_app/current
 PID=$APP_ROOT/tmp/pids/unicorn.pid
 CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
-AS_USER=username
+AS_USER=ec2-user
 set -u
 
 OLD_PIN="$PID.oldbin"
