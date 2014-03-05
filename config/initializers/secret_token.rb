df06325411a483aa9fe0192f4ef1f475fe8f1ce7
Ashish <ashish@ireslab.com>
2014-03-05 15:58:22 +0530

    Inital Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UnicornApp::Application.config.secret_key_base = '5e61658536e3cb8ed1af0cd65c50ac2321c72d327e9ba139947372a4b4ac06fc8dcc4df7136313f55de6eee8268dbb402c808309840ab2ac1c5b5a156b88e41c'
