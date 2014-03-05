05a8f57051acf4edb5b2a83331045e5d65e08a6d
Ashish <ashish@ireslab.com>
2014-03-05 17:20:46 +0530

    scaffold

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,24 +4,12 @@ require "bundler/capistrano"            # install all the new missing plugins...
 require 'capistrano/ext/multistage'     # deploy on all the servers..
 require "rvm/capistrano"                # if you are using rvm on your server..
 require './config/boot'
-#require 'delayed/recipes'               # load this for delayed job..
-#require "whenever/capistrano"
 
-before "deploy:assets:precompile","deploy:config_symlink"
-#before "deploy:update_code",    "delayed_job:stop"  # stop the previous deployed job workers...
 after "deploy:update", "deploy:cleanup" #clean up temp files etc.
 after "deploy:update_code","deploy:migrate"
-after "deploy:create_symlink", "deploy:update_crontab"
-#after "deploy:start",   "delayed_job:start" #start the delayed job
-#after "deploy:restart", "delayed_job:restart" # restart it..
-
-#set :whenever_command, "bundle exec whenever"
 set(:application) { "unicorn_app" }
-#set :delayed_job_args, "-n 2"            # number of delayed job workers
 set :rvm_ruby_string, '2.0.0'             # ruby version you are using...
 set :rvm_type, :user
-#set :whenever_environment, defer { stage }  # whenever gem for cron jobs...
-#set :whenever_identifier, defer { "#{application}_#{stage}" }
 server "ec2-54-235-63-23.compute-1.amazonaws.com", :web, :app, :db, primary: true
 set (:deploy_to) { "/home/ec2-user/#{application}/#{stage}" }
 set :user, 'ec2-user'
@@ -32,7 +20,7 @@ ssh_options[:auth_methods] = ["publickey"]
 ssh_options[:keys] = ["/home/ashish/Desktop/nqlivedev.pem"]
 
 set :keep_releases, 3
-set :repository, "git@bitbucket.org:ashish198907/unicorn_app.git"
+set :repository, "git@github.com:ashish198907/#{application}.git"
 set :use_sudo, false
 set :scm, :git
 default_run_options[:pty] = true
@@ -41,22 +29,11 @@ set :deploy_via, :remote_cache
 set :git_shallow_clone, 1
 set :git_enable_submodules, 1
 
-#namespace :deploy do
-#  task :start do ; end
-#  task :stop do ; end
-#  task :restart, :roles => :app, :except => { :no_release => true } do
-#    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#  end
-#  task :config_symlink do
-#    run "ln -sf #{shared_path}/database.yml #{release_path}/config/database.yml"
-#  end
-#end
-
 namespace :deploy do
   %w[start stop restart].each do |command|
     desc "#{command} unicorn server"
     task command, roles: :app, except: {no_release: true} do
-      run "/etc/init.d/unicorn_#{application} #{command}"
+      run "/etc/init.d/unicorn_#{application}_#{fetch(:stage)} #{command}"
     end
   end
 
